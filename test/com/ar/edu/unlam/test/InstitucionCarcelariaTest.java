9e6a46aa405a6028448d4dfdd9757462393ee065
Kajukai32 <arturojas32@gmail.com>
2026-06-22 17:55:33 -0300

    crear Institucion carcelaria

--- test/com/ar/edu/unlam/test/InstitucionCarcelariaTest.java
+++ test/com/ar/edu/unlam/test/InstitucionCarcelariaTest.java
@@ -5,6 +5,8 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
+import com.ar.edu.unlam.data.model.InstitucionCarcelaria;
+
 public class InstitucionCarcelariaTest {
 
 	@Before
@@ -13,7 +15,13 @@ public class InstitucionCarcelariaTest {
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		//GIVEN
+	String nombre = "El silencio";
+		//WHEN
+	InstitucionCarcelaria ic = new InstitucionCarcelaria(nombre);
+		//THEN
+		assertNotNull(ic);
 	}
+
 
 }
